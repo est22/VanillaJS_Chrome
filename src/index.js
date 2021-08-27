ce20f72659a9470fb1c32be72a8fd94a4efef374
est22 <60957334+est22@users.noreply.github.com>
2021-08-28 02:05:16 +0900

    Vanilla JS Chrome Challenge 5번째 과제 패널 크기 바뀔때마다 색 변경

--- src/index.js
+++ src/index.js
@@ -1,40 +1,17 @@
-// <⚠️ DONT DELETE THIS ⚠️>
 import "./styles.css";
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
-const h2 = document.querySelector("h2");
-// <⚠️ /DONT DELETE THIS ⚠️>
-/*
-✅ The text of the title should change when the mouse is on top of it.
-✅ The text of the title should change when the mouse is leaves it.
-✅ When the window is resized the title should change.
-✅ On right click the title should also change.
-✅ The colors of the title should come from a color from the colors array.
-✅ DO NOT CHANGE .css, or .html files.
-✅ ALL function handlers should be INSIDE of "superEventHandler"
-*/
-const superEventHandler = {
-  handleMouseEnter: function () {
-    h2.innerText = "The mouse is here!";
-    h2.style.color = "#1abc9c";
-  },
 
-  handleMouseLeave: function () {
-    h2.innerText = "The mouse is gone!";
-    h2.style.color = "#3498db";
-  },
+const colors = ["#2E88D5", "#904FAD", "#EEBC10"];
+const hello = document.querySelector("body");
 
-  handleWindowResize: function () {
-    h2.innerText = "You just resized!";
-    h2.style.color = "#9b59b6";
-  },
-
-  handleMouseRightClick: function () {
-    h2.innerText = "That was a right click!";
-    h2.style.color = "#e74c3c";
+function colorHandle() {
+  if (window.innerWidth < 400) {
+    hello.style.backgroundColor = colors[0];
+  } else if (window.innerWidth >= 400 && window.innerWidth < 600) {
+    hello.style.backgroundColor = colors[1];
+  } else {
+    hello.style.backgroundColor = colors[2];
   }
-};
+}
 
-h2.addEventListener("mouseenter", superEventHandler.handleMouseEnter); // click이 일어나면 function 실행
-h2.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
-window.addEventListener("resize", superEventHandler.handleWindowResize);
-window.addEventListener("contextmenu", superEventHandler.handleMouseRightClick);
+colorHandle();
+window.addEventListener("resize", colorHandle);
