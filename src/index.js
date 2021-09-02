c5692664569422d80595943f0e03165621b9b29a
est22 <60957334+est22@users.noreply.github.com>
2021-09-03 00:33:51 +0900

    Give me Color random color background

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
-// index.js
-const clockTitle = document.querySelector(".js-clock");
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34"
+];
+const button = document.querySelector("#press");
+button.addEventListener("click", changeColor);
 
-const clock = setInterval(function () {
-  const xmas = new Date("December 25 2021 00:00:00 GMT+0900").getTime();
-  const now = new Date().getTime();
-  const distance = xmas - now;
+function changeColor() {
+  let color1 = colors[Math.floor(Math.random() * colors.length)];
+  let color2 = colors[Math.floor(Math.random() * colors.length)];
+  console.log(color1, color2);
+  filterDuplicates(color1, color2);
 
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // jS -> HTML 추가
+  const bg = document.querySelector("body");
+  bg.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+  // image를 body 내에 추가
+  // document.body.appendChild(bg); // prepend도 가능
+}
 
-  clockTitle.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+function filterDuplicates(color1, color2) {
+  if (color1 === color2) {
+    while (color1 !== color2) {
+      let color2 = colors[Math.floor(Math.random() * colors.length)];
+      console.log(color1, color2);
+    }
 
-  if (distance < 0) {
-    clearInterval(clock);
-    clockTitle.innerText = `IT'S D-DAY!!`;
+    return color1, color2;
   }
-}, 1000);
+  return;
+}
